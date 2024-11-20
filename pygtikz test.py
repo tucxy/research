773e5e005fe5c47361cbe3c0c8f947c27651de30
Danny Banegas <baneg003@umn.edu>
2024-11-20 05:41:51 -0600

    pygtikz beta version complete

---
+++ pygtikz test.py
@@ -0,0 +1,139 @@
+import networkx as nx
+import sys
+sys.path.append('C:\\Users\\baneg\\OneDrive\\Desktop\\Git\\research\\tuxgraph.py') # here is the path with tuxgraph
+from pygtikz import viz
+from itertools import combinations
+import math
+
+# how to merge two graphs:
+'''
+# Create two graphs
+G1 = nx.Graph()
+G1.add_edges_from([('a', 'b'), ('b', 'c')])
+
+G2 = nx.Graph()
+G2.add_edges_from([('c', 'd'), ('d', 'e')])
+
+# Merge the graphs
+G_merged = nx.compose(G1, G2)
+'''
+#how to merge multiple graphs
+'''
+# Create multiple graphs
+G1 = nx.Graph()
+G1.add_edges_from([('a', 'b'), ('b', 'c')])
+
+G2 = nx.Graph()
+G2.add_edges_from([('c', 'd'), ('d', 'e')])
+
+G3 = nx.Graph()
+G3.add_edges_from([('e', 'f'), ('f', 'g')])
+
+# Merge the graphs
+G_merged = nx.compose_all([G1, G2, G3])
+'''
+
+def inspect(G):
+    nodes = list(G.nodes)
+    edges = list(G.edges)
+    
+    Ginfo = {
+        "Nodes": nodes,
+        "Edges": edges
+    }
+    
+    return Ginfo
+
+def build(vertices, edges):
+    """
+    Create a graph using NetworkX from a list of vertices and edges.
+    build([u,v,w,...], [(u, v), (w, v), ...])
+    """
+    G = nx.Graph()
+    G.add_nodes_from(vertices)
+    G.add_edges_from(edges)
+    return G
+
+def merge(*graphs):
+    """
+    Merge multiple NetworkX graphs into a single graph.
+    """
+    G = nx.Graph()
+    
+    for graph in graphs:
+        G.add_nodes_from(graph.nodes())
+        G.add_edges_from(graph.edges())
+    
+    return G
+
+def path(cycle):
+
+    C = list(cycle)
+    G = nx.Graph()
+    G.add_nodes_from(C)
+    for i in range(len(C) - 1):
+        G.add_edge(C[i], C[i + 1])
+    return G
+
+def cycle(cycle):
+
+    C = list(cycle)
+    G = nx.Graph()
+    G.add_nodes_from(C)
+    for i in range(len(C)):
+        G.add_edge(C[i], C[(i + 1) % len(C)])
+    return G
+
+def star(hub, neighbors):
+
+    leaves = list(neighbors)
+    G = nx.Graph()
+    G.add_node(hub)
+    for node in leaves:
+        G.add_node(node)
+        G.add_edge(hub, node)
+    return G
+
+def K(n):
+    G = nx.Graph()
+    G.add_nodes_from(range(0,n))
+    for node in range(0,n):
+        for neighbor in range(0,n):
+            if node != neighbor:
+                G.add_edge(node,neighbor)
+    return G
+
+def trees(n):
+    def is_isomorphic_to_any(graph, graph_list):
+        for g in graph_list:
+            if nx.is_isomorphic(graph, g):
+                return True
+        return False
+
+    all_trees = []
+    nodes = list(range(n + 1))  # A tree with n edges has n+1 nodes
+
+    for edges in combinations(combinations(nodes, 2), n):
+        G = nx.Graph()
+        G.add_edges_from(edges)
+        if nx.is_tree(G) and not is_isomorphic_to_any(G, all_trees):
+            all_trees.append(G)
+
+    return all_trees
+#notebook
+n = 21
+
+def length(a,b):
+    if type(a)!= int or type(b)!=int:
+        return "NaN"
+    else:
+        return min(abs(a-b),n-abs(a-b))
+
+def custom_edge_length(node1, node2):
+        if isinstance(node1, str) or isinstance(node2, str):
+            return "NaN"
+        return abs(node1 *node2)
+
+G1  = path(["a",3,"sigma male grindset",6,9])
+viz([G1],edge_length_func=custom_edge_length)
+
